9165e15bf28592e5d124940e21984e4dd2c44f6e
TUrielRios <132588891+TUrielRios@users.noreply.github.com>
2024-10-04 19:50:56 -0300

    Update cocktail-menu.jsx

--- components/cocktail-menu.jsx
+++ components/cocktail-menu.jsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import { X } from 'lucide-react'
 import Image from 'next/image'
 import sunnyimg from '../app/public/sunny.jpg'
 import basil from '../app/public/basil.jpg'
