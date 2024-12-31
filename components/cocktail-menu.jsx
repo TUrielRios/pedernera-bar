acc393579edf1154839b5bee220a78eb65513e21
Tiziano <riostiziano6@gmail.com>
2024-12-31 18:42:37 -0300

    update carta

--- components/cocktail-menu.jsx
+++ components/cocktail-menu.jsx
@@ -1,133 +1,168 @@
 'use client'
 
 import { useState } from 'react'
-import { Martini, Wine, X } from 'lucide-react'
+import { X } from 'lucide-react'
 import Image from 'next/image'
-import sunnyimg from '../app/public/sunny.jpg'
-import basil from '../app/public/basil.jpg'
-import daiquiri from '../app/public/daiquiri.jpg'
+import punt_pampa from '../app/public/sunny.jpg'
+import dulce_criollo from '../app/public/dulce_criollo.png'
+import sol_norte from '../app/public/sol_norte.png'
 import frutal from '../app/public/frutal.jpg'
-import ginTonic from '../app/public/gin tonic.jpg'
+import gin_criollo from '../app/public/gin-criollo.png'
 import highball from '../app/public/highball.jpg'
-import short from '../app/public/short.jpg'
+import ron_argento from '../app/public/ron_argento.png'
+import pomelo_tropical from '../app/public/pomelo_tropical.png'
 import footerimg from '../app/public/4glasses.jpg'
+import baileys from '../app/public/baileys.png'
+import patron from '../app/public/patron.png'
+import jagger from '../app/public/jagger.png'
 
 const cocktails = [
   {
     id: 1,
-    name: "Sunny Cocktail",
-    description: "Trago cítrico de la dueña del hogar",
-    image: sunnyimg,
+    name: "Punt & Pampa",
+    description: "Un cóctel amargo-dulce con un final cítrico refrescante.",
+    image: punt_pampa,
     ingredients: [
-      { name: "Base alcoholica", description: "Gin, ron blanco y vermu blanco" },
-      { name: "Citrico", description: "Zumo de limón y naranja" },
-      { name: "Endulzante", description: "Jarabe de azúcar" },
-      { name: "Garnish", description: "Twist de naranja y cereza" },
-      { name: "Extra", description: "Hielo en cubitos" },
-      { name: "Cristaleria", description: "Martini" },
-      { name: "Metodo", description: "Coctelera" },
+      { name: "Base alcoholica", description: "Vermú Punt e Mes" },
+      { name: "Citrico", description: "Jugo de pomelo" },
+      { name: "Endulzante", description: "Granadina" },
+      { name: "Hielo", description: "Cubitos" },
+      { name: "Cristaleria", description: "Vaso corto" },
+      { name: "Metodo", description: "Construcción directa" }
     ]
   },
   {
     id: 2,
-    name: "Tropical Sour",
-    description: "Un cóctel vibrante y refrescante con notas tropicales.",
-    image: frutal,
+    name: "Obrero de la Tarde",
+    description: "Un trago chispeante y equilibrado con notas amargas y dulces.",
+    image: highball,
     ingredients: [
-      { name: "Base alcoholica", description: "Ron añejo" },
-      { name: "Citrico", description: "Zumo de limón" },
-      { name: "Endulzante", description: "Jarabe de miel" },
-      { name: "Fruta", description: "Puré de piña" },
-      { name: "Garnish", description: "Rodaja de piña y hojas de menta" },
-      { name: "Extra", description: "Hielo en cubitos" },
-      { name: "Cristaleria", description: "Copa Old Fashioned" },
-      { name: "Metodo", description: "Coctelera" },
+      { name: "Base alcoholica", description: "Amargo Obrero" },
+      { name: "Citrico", description: "Jugo de pomelo" },
+      { name: "Diluyente", description: "Sprite" },
+      { name: "Hielo", description: "Cubitos" },
+      { name: "Cristaleria", description: "Vaso largo" },
+      { name: "Metodo", description: "Construcción directa" }
     ]
   },
   {
     id: 3,
-    name: "Smoky Old Fashioned",
-    description: "Una reversión del clásico con un toque ahumado.",
-    image: daiquiri,
+    name: "Gintonic Criollo",
+    description: "Un clásico reversionado con aromas cítricos.",
+    image: gin_criollo,
     ingredients: [
-      { name: "Base alcoholica", description: "Bourbon" },
-      { name: "Endulzante", description: "Azúcar moreno" },
-      { name: "Amargo", description: "Bitter de Angostura" },
-      { name: "Extra", description: "Hielo grande" },
-      { name: "Garnish", description: "Twist de naranja quemado" },
-      { name: "Cristaleria", description: "Copa Old Fashioned" },
-      { name: "Metodo", description: "Directo en vaso" },
-      { name: "Toque final", description: "Humo de madera de cerezo" }
+      { name: "Base alcoholica", description: "Gin Tanqueray y 10ml de vermú" },
+      { name: "Diluyente", description: "Agua tónica" },
+      { name: "Citrico", description: "Rodaja de pomelo" },
+      { name: "Hielo", description: "Cubitos" },
+      { name: "Cristaleria", description: "Vaso largo" },
+      { name: "Metodo", description: "Construcción directa" }
     ]
   },
   {
     id: 4,
-    name: "Cucumber Fizz",
-    description: "Un cóctel refrescante con pepino y soda.",
-    image: basil,
+    name: "Roncito Argento",
+    description: "Un trago fresco con un toque cítrico y burbujeante.",
+    image: ron_argento,
     ingredients: [
-      { name: "Base alcoholica", description: "Gin" },
-      { name: "Citrico", description: "Zumo de lima" },
-      { name: "Endulzante", description: "Jarabe de agave" },
-      { name: "Fruta", description: "Pepino fresco" },
-      { name: "Gaseosa", description: "Soda" },
-      { name: "Garnish", description: "Rodaja de pepino y hojas de menta" },
-      { name: "Extra", description: "Hielo en cubitos" },
-      { name: "Cristaleria", description: "Copa Highball" },
-      { name: "Metodo", description: "Coctelera" },
+      { name: "Base alcoholica", description: "Ron Bacardi" },
+      { name: "Citrico", description: "Jugo de limón" },
+      { name: "Endulzante", description: "Almibar" },
+      { name: "Diluyente", description: "Sprite" },
+      { name: "Hielo", description: "Cubitos" },
+      { name: "Cristaleria", description: "Vaso largo" },
+      { name: "Metodo", description: "Construcción directa" }
     ]
   },
   {
     id: 5,
-    name: "Spicy Margarita",
-    description: "Una margarita con un toque picante.",
-    image: highball,
+    name: "Sol del Norte",
+    description: "Un cóctel dulce con un degradado espectacular.",
+    image: sol_norte,
     ingredients: [
-      { name: "Base alcoholica", description: "Tequila blanco" },
-      { name: "Citrico", description: "Zumo de lima" },
-      { name: "Endulzante", description: "Jarabe de agave" },
-      { name: "Picante", description: "Jalapeño fresco" },
-      { name: "Garnish", description: "Rodaja de jalapeño y sal de chile" },
-      { name: "Extra", description: "Hielo triturado" },
-      { name: "Cristaleria", description: "Copa Margarita" },
-      { name: "Metodo", description: "Coctelera" },
+      { name: "Base alcoholica", description: "Vodka" },
+      { name: "Citrico", description: "Jugo de naranja" },
+      { name: "Endulzante", description: "Granadina" },
+      { name: "Hielo", description: "Cubitos" },
+      { name: "Cristaleria", description: "Vaso largo" },
+      { name: "Metodo", description: "Construcción directa" }
     ]
   },
   {
     id: 6,
-    name: "Berry Mule",
-    description: "Una versión frutal del clásico Moscow Mule.",
-    image: ginTonic,
+    name: "Pomelo Tropical",
+    description: "Una mezcla cítrica y refrescante con cachaza.",
+    image: pomelo_tropical,
     ingredients: [
-      { name: "Base alcoholica", description: "Vodka" },
-      { name: "Citrico", description: "Zumo de limón" },
-      { name: "Endulzante", description: "Jarabe de azúcar" },
-      { name: "Fruta", description: "Moras frescas" },
-      { name: "Gaseosa", description: "Ginger beer" },
-      { name: "Garnish", description: "Menta fresca y moras" },
-      { name: "Extra", description: "Hielo en cubitos" },
-      { name: "Cristaleria", description: "Jarra de cobre" },
-      { name: "Metodo", description: "Coctelera" },
+      { name: "Base alcoholica", description: "Cachaza" },
+      { name: "Citrico", description: "Jugo de pomelo" },
+      { name: "Endulzante", description: "Azúcar" },
+      { name: "Diluyente", description: "Sprite" },
+      { name: "Hielo", description: "Cubitos" },
+      { name: "Cristaleria", description: "Vaso largo" },
+      { name: "Metodo", description: "Construcción directa" }
     ]
   },
   {
     id: 7,
-    name: "Rose Negroni",
-    description: "Una variación floral del clásico Negroni.",
-    image: short,
+    name: "Dulces sueños",
+    description: "Un trago cremoso con un toque dulce y un contraste inesperado.",
+    image: dulce_criollo,
     ingredients: [
-      { name: "Base alcoholica", description: "Gin" },
-      { name: "Aperitivo", description: "Campari" },
-      { name: "Vermut", description: "Vermú rosado" },
-      { name: "Garnish", description: "Pétalos de rosa comestibles" },
-      { name: "Extra", description: "Hielo grande" },
-      { name: "Cristaleria", description: "Copa Old Fashioned" },
-      { name: "Metodo", description: "Directo en vaso" }
+      { name: "Base alcoholica", description: "Baileys" },
+      { name: "Endulzante", description: "Granadina" },
+      { name: "Refuerzo", description: "Vodka" },
+      { name: "Hielo", description: "Cubitos" },
+      { name: "Cristaleria", description: "Vaso corto" },
+      { name: "Metodo", description: "Agitado" }
+    ]
+  },
+  {
+    id: 9,
+    name: "El Patrón del Obrero",
+    description: "Un trago fresco e intenso con notas herbales.",
+    image: patron,
+    ingredients: [
+      { name: "Base alcoholica", description: "Amargo Obrero" },
+      { name: "Citrico", description: "Jugo de limón y pomelo" },
+      { name: "Diluyente", description: "Soda" },
+      { name: "Hielo", description: "Cubitos" },
+      { name: "Cristaleria", description: "Vaso corto" },
+      { name: "Metodo", description: "Construcción directa" }
+    ]
+  },
+  {
+    id: 10,
+    name: "Fresco y Jagger",
+    description: "Un trago herbáceo y refrescante con un toque oscuro.",
+    image: jagger,
+    ingredients: [
+      { name: "Base alcoholica", description: "Jagger" },
+      { name: "Cítrico", description: "Jugo de pomelo" },
+      { name: "Endulzante", description: "Almíbar" },
+      { name: "Diluyente", description: "Sprite" },
+      { name: "Hielo", description: "Cubitos" },
+      { name: "Cristaleria", description: "Vaso corto" },
+      { name: "Metodo", description: "Construcción directa" }
+    ]
+  },
+  {
+    id: 11,
+    name: "Argentum",
+    description: "Un cóctel complejo con un balance perfecto entre dulce, amargo y cítrico.",
+    image: frutal,
+    ingredients: [
+      { name: "Base alcoholica", description: "Ron Bacardi" },
+      { name: "Refuerzo", description: "Amargo Obrero" },
+      { name: "Citrico", description: "Jugo de pomelo" },
+      { name: "Endulzante", description: "Granadina" },
+      { name: "Hielo", description: "Cubitos" },
+      { name: "Cristaleria", description: "Vaso largo" },
+      { name: "Metodo", description: "Agitado y colado" }
     ]
   }
-  
+];
 
-]
 
 export function CocktailMenu() {
   const [selectedCocktail, setSelectedCocktail] = useState(null)
